0db9f5eef73a3b5df5e815479349d937f0c5ed2c
PavelDevelop <paveldsirius@gmail.com>
2017-10-29 19:54:30 +0300

    2 lecture

---
+++ lecture2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        // MSDN, Metanit.com - Множество туториалов, понятная документация!!
+        static void Main(string[] args)
+        {
+            var weapons = new List<Weapon>
+            {
+                new Gun(),
+                new Knife(),
+                new Stick(),
+                new NuclearWeapon(),
+                new WaterGun()
+            };
+
+            foreach (var weapon in weapons.OrderByDescending(x => x.Damage))
+            {
+                weapon.Attack();
+
+                if (weapon is IChangeDamagable)
+                {
+                    var weaponChangeDamagable = weapon as IChangeDamagable;
+                    weaponChangeDamagable.ChangeDamage();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lecture2/ConsoleApp1/ConsoleApp1/Weapon.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public abstract class Weapon
+    {
+        public string BrandName { get; protected set; }
+        public IChangeDamageStrategy Damage { get; protected set; }
+        public float CriticalChance { get; set; }
+
+        public abstract void Attack();
+    }
+
+    public interface IChangeDamageStrategy
+    {
+        int GetDamage();
+    }
+
+    public interface IChangeDamagable
+    {
+        void ChangeDamage();
+    }
+
+    public class TenPointsChangeDamageStrategy : IChangeDamageStrategy
+    {
+        public int GetDamage()
+        {
+            return 10;
+        }
+    }
+
+    public class TwentyPointsChangeDamageStrategy : IChangeDamageStrategy
+    {
+        public int GetDamage()
+        {
+            return 20;
+        }
+    }
+
+    public class ZeroPointsChangeDamageStrategy : IChangeDamageStrategy
+    {
+        public int GetDamage()
+        {
+            return 0;
+        }
+    }
+
+    public class Gun : Weapon, IChangeDamagable
+    {
+        public Gun()
+        {
+            BrandName = "Валера";
+            Damage = new TenPointsChangeDamageStrategy();
+            CriticalChance = 0.25f;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("Пиф паф");
+        }
+
+        public void ChangeDamage()
+        {
+            Console.WriteLine($"{Damage} урона");
+        }
+    }
+
+    public class Knife : Weapon, IChangeDamagable
+    {
+        public Knife()
+        {
+            BrandName = "Милана";
+            Damage = new TenPointsChangeDamageStrategy();
+            CriticalChance = 0.5f;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("Шинк шинк");
+        }
+
+        public void ChangeDamage()
+        {
+            Console.WriteLine($"{Damage} урона");
+        }
+    }
+
+    public class NuclearWeapon : Weapon, IChangeDamagable
+    {
+        public NuclearWeapon()
+        {
+            BrandName = "Чебурашка";
+            Damage = new TwentyPointsChangeDamageStrategy();
+            CriticalChance = 1f;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("БООООМ БОМ");
+        }
+
+        public void ChangeDamage()
+        {
+            Console.WriteLine($"{Damage} урона");
+        }
+    }
+
+    public class Stick : Weapon, IChangeDamagable
+    {
+        public Stick()
+        {
+            BrandName = "Люси";
+            Damage = new TwentyPointsChangeDamageStrategy();
+            CriticalChance = 1f;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("ШМЯК");
+        }
+
+        public void ChangeDamage()
+        {
+            Console.WriteLine($"{Damage} урона");
+        }
+    }
+
+    
+    public class WaterGun : Weapon
+    {
+        public WaterGun()
+        {
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("Льется вода");
+        }
+    }
+}
